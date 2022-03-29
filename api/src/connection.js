fe8bcacf7284b7abd9581a145d0b3d4f4f511342
Zeljko <zeljko.kubicko@gmail.com>
2022-03-29 06:04:15 +0200

    env

--- api/src/connection.js
+++ api/src/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config()
 
 //const connection = "mongodb://mongo:27017/test_task";
-const connection = "mongodb+srv://Kuba:Kuba10@testdb.cwsja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const connection = `mongodb+srv://Kuba:${REACT_APP_P}@testdb.cwsja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const connection = `mongodb+srv://Kuba:${process.env.REACT_APP_PASS}@testdb.cwsja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const connectDb = () => {
   return mongoose.connect(connection);
